4a8b89ec4dcac9c8f2c4164daa64e11d436957fc
mrtomyum <mrtomyum@gmail.com>
2018-06-06 14:02:12 +0700

    update open database

--- main.go
+++ main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"github.com/mrtomyum/nopadol/mariadb"
-	"github.com/mrtomyum/nopadol/sale/service"
+	"github.com/mrtomyum/nopadol/incentive"
 )
 
+const (
+	dbUser = "root"
+	dbPass = "[ibdkifu88"
+	dbAddress = "http://nopadol.net:3306"
+	dbName = "nopadol"
+)
 
 func main() {
+	conn := dbUser + ":" + dbPass + "@tcp(" + dbAddress + ")/" + dbName
+	db, err := sql.Open("mysql", conn)
+	must(err)
+	defer db.Close()
+
 	// init repos
-	saleRepo := mariadb.NewWebRepository()
+	saleRepo := mariadb.NewIncentiveRepository()
 
 	// init services
-	saleService := saleService.New(saleRepo)
+	saleService := sale.Service.New(saleRepo)
 
 	// init
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println("Error:", err)
+		log.Fatal(err)
+	}
 }
