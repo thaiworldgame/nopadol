5ebccd145a7567fab3d3315e658d54a964eed3c3
satit13 <satit@nopadol.com>
2018-07-04 12:57:33 +0700

    enabled cors

--- sale/transport.go
+++ sale/transport.go
@@ -79,4 +79,6 @@ func NewHTTPTransport(ep Endpoint) http.Handler {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
