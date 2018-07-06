3d6c6af1b74cde796e6e58e797a20d3bca7ca901
satit13 <satit@nopadol.com>
2018-07-06 13:33:45 +0700

    call from postgres db fix date to '2018-07-05'

--- postgres/delivery.go
+++ postgres/delivery.go
@@ -1,12 +1,14 @@
 package postgres
 
 import (
-	"github.com/mrtomyum/nopadol/delivery"
 	"database/sql"
 	_ "github.com/lib/pq"
+	"github.com/mrtomyum/nopadol/delivery"
 	"log"
 	//"google.golang.org/genproto/googleapis/type/date"
 	"fmt"
+	//"time"
+	"time"
 )
 
 func NewDeliveryRepository(db *sql.DB) delivery.Repository {
@@ -15,30 +17,73 @@ func NewDeliveryRepository(db *sql.DB) delivery.Repository {
 
 type deliveryRepository struct{ db *sql.DB }
 
-func (d *deliveryRepository)ReportDaily() (interface{}, error) {
+func (d *deliveryRepository) ReportDaily() (interface{}, error) {
 	log.Println("begin postgres.ReportDaily")
 	type doModel struct {
-		Docno    string
-		Docdate  sql.NullString `db:"docdate"`
-		Datesend sql.NullString `db:"datesend"`
-		Timesend sql.NullString `db:"timesend"`
+		Id          sql.NullInt64
+		DoDocno     sql.NullString `json:"do_docno"`
+		SoNo        sql.NullString `json:"so_no"`
+		ConfirmDate sql.NullString `json:"confirm_date"`
+		DoDate      sql.NullString `json:"do_date"`
+		DiffDate    int64          `json:"diff_date"`
+		Description sql.NullString `json:"description"`
+		ArName      sql.NullString `json:"ar_name"`
 	}
 
-	_do := doModel{}
-	lccommand := "select docno,docdate,datesend,timesend from sm_do.tb_do_delivery limit 1 "
-	rs := d.db.QueryRow(lccommand)
-	//_docno := ""
-	err := rs.Scan(&_do.Docno, &_do.Docdate, &_do.Datesend, &_do.Timesend)
-	//err := rs.Scan(&_do.docno)
-	if err != nil {
-		return nil, err
+	type doResponse struct {
+		Id          int64
+		DoDocno     string `json:"do_docno"`
+		SoNo        string `json:"so_no"`
+		ConfirmDate time.Time `json:"confirm_date"`
+		DoDate      time.Time `json:"do_date"`
+		DiffDate    int64          `json:"diff_date"`
+		Description string `json:"description"`
+		ArName      string `json:"ar_name"`
 	}
+	_do := doModel{}
+	_dos := []doResponse{}
+	_doResponse := doResponse{}
+
+	lccommand := `select * from sm_do.doreport('2018-07-05')`
+
+	rs, err := d.db.Query(lccommand)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	for rs.Next() {
+		err := rs.Scan(&_do.Id, &_do.DoDocno, &_do.SoNo, &_do.ConfirmDate, &_do.DoDate, &_do.DiffDate, &_do.Description, &_do.ArName)
+		if err != nil {
+			return nil, err
+		}
+
+		_doResponse.Id = _do.Id.Int64
+		_doResponse.DoDocno = _do.DoDocno.String
+		_doResponse.SoNo = _do.SoNo.String
+
+		layout := "2006-01-02T15:04:05.000Z"
+		str := _do.ConfirmDate.String
+		t, err := time.Parse(layout, str)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		//fmt.Println(t)
+		_doResponse.ConfirmDate = t
+
+		str = _do.DoDate.String
+		t, err = time.Parse(layout, str)
+		_doResponse.DoDate = t
+		_doResponse.DiffDate = _do.DiffDate
+		_doResponse.Description = _do.Description.String
+		_doResponse.ArName = _do.ArName.String
+		_dos = append(_dos, _doResponse)
+		fmt.Println(_doResponse)
+	}
+
 	fmt.Println(_do)
 	return map[string]interface{}{
-		"doc_no" : _do.Docno,
-		"doc_date" : _do.Docdate.String,
-		"date_send": _do.Datesend.String,
-		"time_send": _do.Timesend.String,
+		"data":_dos,
 	}, nil
+
 	//return nil, fmt.Errorf("error make mannual")
 }
