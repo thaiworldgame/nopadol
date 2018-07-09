d352fc63595d3d3d4aaa537e73fb867c92926059
itnopadol <it@nopadol.com>
2018-07-09 10:08:11 +0700

    Edit Insert POS

--- pos/service.go
+++ pos/service.go
@@ -1,8 +1,34 @@
 package pos
 
-import "context"
+import "fmt"
+
+func New(repo Repository) (Service) {
+	return &service{repo}
+}
+
+type service struct {
+	repo Repository
+}
 
 type Service interface {
-	New(context.Context, *NewPosTemplate) (NewPosResponseTemplate, error)
-	SearchById(ctx context.Context, request *SearchPosByIdRequestTemplate) (SearchPosByIdResponseTemplate, error)
+	Create(req *NewPosTemplate) (interface{}, error)
+	SearchById(req *SearchPosByIdRequestTemplate) (interface{}, error)
+}
+
+func (s *service)Create(req *NewPosTemplate) (interface{}, error){
+	fmt.Println("Service")
+	resp, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (s *service)SearchById(req *SearchPosByIdRequestTemplate)(interface{}, error){
+	resp, err := s.repo.SearchById(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
