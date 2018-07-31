d84e3a4e43bc86ca888c43afecdbd9cadb55e0aa
itnopadol <it@nopadol.com>
2018-07-31 14:07:31 +0700

    edit pos

---
+++ print/transport.go
@@ -0,0 +1,18 @@
+package printservice
+
+import "net/http"
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Content-Type", "application/json")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func MakeHandler(s Service) http.Handler{
+
+}
