d7fe543540c731685b40f458d78324358342fd8d
satit13 <satit@nopadol.com>
2018-12-26 11:48:26 +0700

    prepare to merge master

---
+++ sqldb/db.go
@@ -0,0 +1,22 @@
+package sqldb
+
+import (
+	"github.com/mrtomyum/nopadol/incentive"
+	"context"
+)
+
+// NewDomain1Repository creates domain1 repository implements by domain4
+func NewIncentiveRepository() incentive.Repository {
+	return &incentiveRepository{}
+}
+
+type incentiveRepository struct {}
+
+func (incentiveRepository) Register(ctx context.Context, entity *incentive.Entity1) (string, error) {
+	return "", nil
+}
+
+// SetField3 sets field3 for Entity1
+func (incentiveRepository) SetField3(ctx context.Context, entityID string, field3 int) error {
+	return nil
+}
