6f0922fc45a065c8334b5ebb43f8274655f3df70
itnopadol <it@nopadol.com>
2019-01-18 15:59:48 +0700

    API UserLOgIn19012019

--- drivethru/endpoint.go
+++ drivethru/endpoint.go
@@ -28,7 +28,7 @@ type (
 	UserLogInRequest struct {
 		UserCode     string `json:"user_code"`
 		Password     string `json:"password"`
-		BranchId     int64  `json:"branch_id"`
+		BranchId     int    `json:"branch_id"`
 		ServerName   string `json:"server_name"`
 		DataBaseName string `json:"data_base_name"`
 	}
@@ -42,50 +42,49 @@ func makeListCompany(s Service) interface{} {
 			return nil, fmt.Errorf(err.Error())
 		}
 
-
 		return map[string]interface{}{
 			"response": map[string]interface{}{
-				"process":"Search Brand",
-				"processDesc":"Success",
-				"isSuccess":true,
+				"process":     "Search Brand",
+				"processDesc": "Success",
+				"isSuccess":   true,
 			},
 			"data": resp,
 		}, nil
 	}
 }
 
-func makeListMachine(s Service) interface{}{
-	return func(ctx context.Context)(interface{}, error){
-		resp,err := s.SearchListMachine()
+func makeListMachine(s Service) interface{} {
+	return func(ctx context.Context) (interface{}, error) {
+		resp, err := s.SearchListMachine()
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 		return map[string]interface{}{
 			"response": map[string]interface{}{
-				"process":"List Pos Machine",
-				"processDesc":"Success",
-				"isSuccess":true,
+				"process":     "List Pos Machine",
+				"processDesc": "Success",
+				"isSuccess":   true,
 			},
 			"data": resp,
 		}, nil
 	}
 }
 
-func makeSearchCarBranch(s Service) interface{}{
+func makeSearchCarBranch(s Service) interface{} {
 	type request struct {
 		Keyword string `json:"keyword"`
 	}
-	return func(ctx context.Context,req *request)(interface{},error){
-		fmt.Println("endpoint keyword is =>",req.Keyword)
-		resp,err := s.SearchCarBrand(req.Keyword)
+	return func(ctx context.Context, req *request) (interface{}, error) {
+		fmt.Println("endpoint keyword is =>", req.Keyword)
+		resp, err := s.SearchCarBrand(req.Keyword)
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 		return map[string]interface{}{
 			"response": map[string]interface{}{
-				"process":"Search car brand",
-				"processDesc":"Success",
-				"isSuccess":true,
+				"process":     "Search car brand",
+				"processDesc": "Success",
+				"isSuccess":   true,
 			},
 			"data": resp,
 		}, nil
@@ -104,55 +103,56 @@ func makeSearchCustomer(s Service) interface{} {
 		}
 		return map[string]interface{}{
 			"response": map[string]interface{}{
-				"process":"Search customer",
-				"processDesc":"Success",
-				"isSuccess":true,
+				"process":     "Search customer",
+				"processDesc": "Success",
+				"isSuccess":   true,
 			},
 			"data": resp,
 		}, nil
 	}
 }
 
-func makeItemSearch(s Service) interface{}{
+func makeItemSearch(s Service) interface{} {
 	type request struct {
 		Keyword string `json:"keyword"`
 	}
-	return func(ctx context.Context, req *request) (interface{},error){
-		fmt.Println("start endpoint item search keyword is => ",req.Keyword)
+	return func(ctx context.Context, req *request) (interface{}, error) {
+		fmt.Println("start endpoint item search keyword is => ", req.Keyword)
 		resp, err := s.SearchItem(req.Keyword)
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 		return map[string]interface{}{
 			"response": map[string]interface{}{
-				"success":true,
-				"error":false,
-				"message":"",
+				"success": true,
+				"error":   false,
+				"message": "",
 			},
 			"item": resp,
 		}, nil
 	}
 }
 
-
-func userLogIn(s Service) interface{}{
-	return func(ctx context.Context, req *UserLogInRequest) (interface{},error){
-		fmt.Println("start endpoint userlogin usercode is => ",req.UserCode)
-		resp, err := s.UserLogIn(req)
+func userLogIn(s Service) interface{} {
+	return func(ctx context.Context, req *UserLogInRequest) (interface{}, error) {
+		fmt.Println("start endpoint userlogin usercode is => ", req.UserCode)
+		resp, err := s.UserLogIn(&UserLogInRequest{BranchId: req.BranchId, UserCode: req.UserCode, Password: req.Password})
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
-		return map[string]interface{}{
-			"response": map[string]interface{}{
-				"process":"login",
-				"processDesc":"successful",
-				"isSuccess":true,
-			},
-			"accessToken":"",
-			"accessDatetime":"",
-			"pathPHPUpload":"http://qserver.nopadol.com/drivethru/upload.php",
-			"pathFile":"http://qserver.nopadol.com/drivethru/tmp/",
-			"user": resp,
-		}, nil
+		//return map[string]interface{}{
+		//	"response": map[string]interface{}{
+		//		"process":     "login",
+		//		"processDesc": "successful",
+		//		"isSuccess": resp,
+		//	},
+		//	"accessToken":    "",
+		//	"accessDatetime": "",
+		//	"pathPHPUpload":  "http://qserver.nopadol.com/drivethru/upload.php",
+		//	"pathFile":       "http://qserver.nopadol.com/drivethru/tmp/",
+		//	"user":           resp,
+		//}, nil
+
+		return resp, nil
 	}
 }
