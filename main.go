d26d18894c7db96fc61f2d02c4e5685cd1fb16e0
satit chomwattana <satit@nopadol.com>
2019-01-21 10:39:23 +0700

    update code

--- main.go
+++ main.go
@@ -214,7 +214,7 @@ func main() {
 
 	//mux.Handle("/p9/",http.StripPrefix("/p9/v1", p9service.MakeHandler(p9Service)))
 	//mux.Handle("/pointofsale/",http.StripPrefix("/pointofsale/v1", pointofsaleservice.MakeHandler(pointofsaleService)))
-
+	fmt.Println("Waiting for Accept Connection :  9999")
 	http.ListenAndServe(":9999", mux)
 }
 
