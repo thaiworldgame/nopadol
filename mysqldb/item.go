7d68003f729f6ff69d0c5953e05bb124cfcfc059
satit chomwattana <satit@nopadol.com>
2019-01-28 16:59:53 +0700

    add Test_ItemRateInsUpd

--- mysqldb/item.go
+++ mysqldb/item.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/mrtomyum/nopadol/product"
 	"fmt"
+	"log"
 )
 
 type itemModel struct {
@@ -63,7 +64,16 @@ type packingRate struct {
 }
 
 func (it *itemModel) save(db *sqlx.DB) (newID int64, err error) {
+
 	return -1, nil
+}
+
+func (it *itemModel) getItemIDbyCode(db *sqlx.DB,code string)(int64,error){
+	lccommand := `select id from Item where code=?`
+	db.QueryRow(lccommand,code).Scan(&it.Id)
+	fmt.Printf("unit id from code %s is %v \n",code,it.Id)
+	return it.Id,nil
+
 }
 
 func (pr *priceModel) save(db *sqlx.DB) (newID int64, err error) {
@@ -74,13 +84,46 @@ func (b *barcodeModel) save(db *sqlx.DB) (newID int64, err error) {
 	return -1, nil
 }
 
+func (r *packingRate)getUnitIDByUnitCode(db *sqlx.DB,unitcode string)(int64,error){
+	lccommand := `select id from ItemRate where unit_code=?`
+	db.QueryRow(lccommand,unitcode).Scan(&r.UnitID)
+	fmt.Printf("unit id from code %s is %v \n",unitcode,r.UnitID)
+	return r.UnitID,nil
+}
 func (r *packingRate) save(db *sqlx.DB) (newID int64, err error) {
+	// check data before ins
+	if r.ItemID == 0 {
+		it := itemModel{}
+		r.ItemID,err = it.getItemIDbyCode(db,r.ItemCode)
+		if err != nil {
+			return -1 , fmt.Errorf("no item_id! ")
+		}
+	}
 
-	lccommand := `insert into ItemRate (item_id,item_code,unit_code,rate1)
-			values (?,?,?,?)`
-	rs,err := db.Exec(lccommand,r.ItemID,r.ItemCode,r.UnitCode,r.RatePerBaseUnit)
+	if r.UnitID == 0 {
+		r.UnitID,err = r.getUnitIDByUnitCode(db,r.UnitCode)
+		if err != nil {
+			return -1 , fmt.Errorf("no unit_id! ")
+		}
+	}
+
+	lccommand := `insert into ItemRate (
+		item_id,
+		item_code,
+		unit_code,
+		rate1
+		) VALUES(?,?,?,?)
+	 ON DUPLICATE KEY UPDATE
+	 rate1=?,unit_id=?`
+
+	rs,err := db.Exec(lccommand,r.ItemID,r.ItemCode,r.UnitCode,r.RatePerBaseUnit,r.RatePerBaseUnit,r.UnitID)
+	if err != nil {
+		log.Printf("sql command %v",err.Error())
+		return -1,err
+	}
 	newID , err = rs.LastInsertId()
 	if err != nil {
+		log.Printf("error insert new rate to item ",err.Error())
 		return -1,err
 	}
 	return newID, nil
