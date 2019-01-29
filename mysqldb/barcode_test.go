dc53bdf7317d06bbd993e864b29ddc2994c06651
satit chomwattana <satit@nopadol.com>
2019-01-29 16:07:26 +0700

    add unit_test item_test.go

---
+++ mysqldb/barcode_test.go
@@ -0,0 +1,45 @@
+package mysqldb
+
+import (
+	"github.com/mrtomyum/nopadol/product"
+	"time"
+	"testing"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_InsUpd_Barcode(t *testing.T) {
+	testDB, err := ConnectDB("demo")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	// build request exits price in demo database
+	req := product.BarcodeTemplate{
+		ItemID:    5244,
+		UnitID:    20,
+		Barcode:   "xxxxx",
+		CompanyID: 1,
+	}
+	fmt.Println("req data ", req)
+	bar := barcodeModel{
+		ItemID: req.ItemID,
+		//ItemID:int64(0),
+		BarCode: req.Barcode,
+		//BarCode:      "",
+		UnitID:       req.UnitID,
+		CreateBy:     "",
+		EditBy:       "",
+		ActiveStatus: 1,
+		CompanyID:    req.CompanyID,
+		ItemCode:     "2120250",
+		UnitCode:     "กระป๋อง",
+	}
+
+	bar.CreateTime.Time = time.Now()
+	bar.EditTime.Time = time.Now()
+	fmt.Println("bar object before save ", bar)
+	_, err = bar.save(testDB)
+	assert.Nil(t, err, nil)
+
+}
