756fd5f1d144d547eee75035598133db88c78f36
satit chomwattana <satit@nopadol.com>
2019-05-03 11:45:07 +0700

    add test to repository customer.go

--- mysqldb/sync_test.go
+++ mysqldb/sync_test.go
@@ -1,23 +1,12 @@
 package mysqldb
 
 import ("testing"
-	"github.com/jmoiron/sqlx"
+	//"github.com/jmoiron/sqlx"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 )
 
 
-func connectDemo(dbName string) (db *sqlx.DB, err error) {
-	fmt.Println("Connect MySql")
-	dsn := "root:[ibdkifu88@tcp(nopadol.net:3306)/" + dbName + "?parseTime=true&charset=utf8&loc=Local"
-	//fmt.Println(dsn,"DBName =", dbName)
-	db, err = sqlx.Connect("mysql", dsn)
-	if err != nil {
-		fmt.Println("sql error =", err)
-		return nil, err
-	}
-	return db, err
-}
 func Test_sync_get_qoutation(t *testing.T){
 	db,err := connectDemo("npdl")
 	if err != nil {
